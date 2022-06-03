ccd00793c986f5b3bff4400dd9c1bd7206d88577
Hannah Kolbeck <hskolbeck@gmail.com>
2022-06-03 09:33:13 -0700

    Add auth snippet

---
+++ auth.js
@@ -0,0 +1,12 @@
+const twitterV2 = require("twitter-api-v2");
+
+const config = {
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    apiKey: process.env.TWITTER_CONSUMER_KEY,
+    apiSecret: process.env.TWITTER_CONSUMER_SECRET,
+};
+
+const client = new twitterV2.TwitterApi({ appKey: config.apiKey, appSecret: config.apiSecret });
+const authLink = await client.generateAuthLink()
+console.log(JSON.stringify(authLink))
